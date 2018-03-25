bc9a3bfabdbedcb2d7a77e76ffc3333695e7feeb
Soumya Deb <debloper@gmail.com>
2018-03-26 02:06:54 +0530

    feat(app): integrate ~~Element UI~~ Buefy as UI framework (amended fork)

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="home">
-    <img src="/img/logo.png">
-    <h1>Welcome to xplex dashboard</h1>
-  </div>
+  <section class="section">
+    <nav class="navbar level container is-widescreen" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand level-left">
+        <a href="https://xplex.me" target="_blank">xplex</a>
+      </div>
+      <div class="navbar-menu level-right">
+        <i class="mdi mdi-menu mdi-36px"></i>
+      </div>
+    </nav>
+    <div class="main container is-widescreen">
+      <img class="xplex-logo" src="/img/logo.png">
+      <h1>Welcome to xplex dashboard!</h1>
+      <a class="button is-link is-rounded is-large">
+        <span>Come on in</span>
+        <span class="icon">
+          <i class="mdi mdi-arrow-right mdi-24px"></i>
+        </span>
+      </a>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -12,4 +28,40 @@ export default {
 </script>
 
 <style scoped lang="scss">
+nav {
+  .navbar-brand {
+    font-size: 2rem;
+    a {
+      color: #6b4e71;
+      text-decoration: none;
+      font-weight: bold;
+      font-family: "Unica One", sans-serif;
+      &:hover, &:focus {
+        color: #1f252b;
+      }
+      &:active {
+        color: #222;
+      }
+    }
+  }
+}
+.main {
+  .xplex-logo {
+    width: 192px;
+    margin: 1rem;
+  }
+  h1 {
+    font-size: 2rem;
+    margin: 1rem auto;
+  }
+  .button.is-link {
+    background-color: #6b4e71;
+    &:hover, &:focus {
+      background-color: #1f252b;
+    }
+    &:active {
+      background-color: #222;
+    }
+  }
+}
 </style>
